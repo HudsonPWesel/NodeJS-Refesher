da6a0b943b37bda109d8654a6a653c0ed1c4b4bb
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-10-07 12:28:19 -0400

    Practice installing and using Express-Basics

--- 02--ExpressJS/app.js
+++ 02--ExpressJS/app.js
@@ -1,33 +1,15 @@
-const http = require('http');
-const routes = require('./routes');
-
 const express = require('express');
-const { lchown } = require('fs');
+const http = require('http');
 
 const app = express();
-// Requests are funneled through many functions which allows you to split your code and easily hook in other middleware functions
-// Function that runs for every request
 
-// Would always run
-// app.use('/', (req, res, next) => {
-// console.log('Mid 2');
-// Automatically sets HTML head
-// res.send('<h1>Hello</h1>');
-// });
-
-app.use('/add-product', (req, res, next) => {
-	// next function to go to next middleware function
-	//
-	console.log('Mid 1');
+app.use('/user', (req, res, next) => {
+	res.send('<h1>Welcome Users</h1>');
 });
 
-// Catch-all route
 app.use('/', (req, res, next) => {
-	console.log('Mid 2');
-	// Automatically sets HTML head
-	res.send('<h1>Hello</h1>');
+	res.send('<h1>Welcome!</h1>');
 });
-
 const server = http.createServer(app);
 
 server.listen(3000);
