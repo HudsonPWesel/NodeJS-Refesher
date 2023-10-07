078c781a4299028d38da706c6de87491c4202a59
Wyld7K <82008705+Wyld7K@users.noreply.github.com>
2023-10-07 12:23:35 -0400

    Handling Routes

--- 02--ExpressJS/app.js
+++ 02--ExpressJS/app.js
@@ -8,14 +8,21 @@ const app = express();
 // Requests are funneled through many functions which allows you to split your code and easily hook in other middleware functions
 // Function that runs for every request
 
-app.use((req, res, next) => {
+// Would always run
+// app.use('/', (req, res, next) => {
+// console.log('Mid 2');
+// Automatically sets HTML head
+// res.send('<h1>Hello</h1>');
+// });
+
+app.use('/add-product', (req, res, next) => {
 	// next function to go to next middleware function
 	//
 	console.log('Mid 1');
-	next();
 });
 
-app.use((req, res, next) => {
+// Catch-all route
+app.use('/', (req, res, next) => {
 	console.log('Mid 2');
 	// Automatically sets HTML head
 	res.send('<h1>Hello</h1>');
